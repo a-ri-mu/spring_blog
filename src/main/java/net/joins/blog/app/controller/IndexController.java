18dad92162b73c4d79dfafeb56e8ead3ddac07b1
a-ri-mu <arumu215@gmail.com>
2021-05-06 23:13:10 +0900

    gitignore setting and index setting

--- src/main/java/net/joins/blog/app/controller/IndexController.java
+++ src/main/java/net/joins/blog/app/controller/IndexController.java
@@ -1,34 +1,69 @@
 package net.joins.blog.app.controller;
 
-import net.joins.blog.common.util.DateTimeUtils;
-import net.joins.blog.web.vo.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.java.Log;
+import net.joins.blog.domain.entity.Post;
+import net.joins.blog.domain.service.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Arrays;
+import java.util.List;
 
+@Log
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/")
 public class IndexController extends BaseController {
 
-    @GetMapping
-    public String index(Model model) {
-        model.addAttribute("name", "hello misolab");
-        model.addAttribute("list", Arrays.asList(1, 2, 3, 4));
+    final PostService service;
+
+    @GetMapping("index.html")
+    public String index(Model model, @PageableDefault(sort = "id", direction = Sort.Direction.DESC , value = 10) Pageable pageable){
+        log.info("index");
+/*
+        List<Post> list = IntStream.range(1,10).mapToObj(i-> Post.builder().link("post.html")
+                .title("제목 - "+i).subtitle("subtitle - "+i).writer("misolob").updated("2021-02-08").build()).collect(Collectors.toList());
+ */
+        Page<Post> list = service.getPostList(pageable);
+
+        model.addAttribute("list", list);
         return "index";
     }
 
-    @GetMapping("/api")
-    public ResponseEntity<Object> index() {
-        ApiResponse response = ApiResponse.of()
-                .add("message", "This is api module")
-                .add("current", DateTimeUtils.toString(System.currentTimeMillis(), "yyyy-MM-dd HH:mm:ss"));
-        return response.toResponseEntity();
+    @GetMapping("post.html")
+    public String post(Model model, Long id){
+        log.info("post");
+        /*
+        Post post = Post.builder().title("Man must explore, and")
+                .subtitle("Problems look mighty smaill from...")
+                .writer("misolab")
+                .updated("2021-02-08")
+                .content("Welcome!!<img class=\"img-gluid\" src=\"img/post-sample-image.jpg\">")
+                .bgImage("img/post-bg.img")
+                .build();
+         */
+        Post post = service.getPost(2L);
+
+        model.addAttribute("post", post);
+
+        return "post";
     }
 
+    @GetMapping("{postId}")
+    public String postId(Model model, @PathVariable Long postId){
+        log.info("post");
+
+        Post post = service.getPost(postId);
+
+        model.addAttribute("post", post);
+
+        return "post";
+    }
 }
